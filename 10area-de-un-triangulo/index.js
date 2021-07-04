46ad191d75d1ca2937c3e01f5be890efc98721bd
unknown <sofiabernabeicejas@gmail.com>
2021-07-04 16:19:27 -0300

    ejercicio 10 area del triangulo

---
+++ 10area-de-un-triangulo/index.js
@@ -0,0 +1,3 @@
+let baseTriangulo = prompt("Ingresá el valor de la base del triángulo.");
+let alturaTriangulo = prompt("Ingresá el valor de la altura del triángulo.");
+alert("El resultado del area del triangulo es: " + ((baseTriangulo * alturaTriangulo)/2));
