e0bcca75dcb801fc726e2dc4e68104f25d76de62
unknown <sofiabernabeicejas@gmail.com>
2021-07-04 16:38:03 -0300

    ejercicio 12 porcentaje

---
+++ 12porcentaje/index.js
@@ -0,0 +1,3 @@
+let numero = prompt ("Ingresa un número");
+let porcentaje = prompt("Ingresa un porcentaje");
+alert("El porcentaje " + porcentaje + "% del numero " + numero + " es: " + ((numero * porcentaje)/ 100))
