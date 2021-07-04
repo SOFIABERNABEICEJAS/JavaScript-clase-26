5e836fb7c1de9311d3b3b9c854ee6fd0e2a6a9a2
unknown <sofiabernabeicejas@gmail.com>
2021-07-04 16:05:21 -0300

    ejercicio 9

---
+++ 9kilometros-a-millas/index.js
@@ -0,0 +1,2 @@
+let kilometros = prompt("Ingresa una cantidad de kilometros por favor:");
+alert("El resultado de la conversión de " + kilometros + " kilometros a millas es: " + (kilometros * 0,621371) + " millas.");
