25e87905ab149f9bc35be3fbddc4645628f4fa49
unknown <sofiabernabeicejas@gmail.com>
2021-07-04 16:28:32 -0300

    ejercicio 11 perimetro del rectandulo

---
+++ 11perimetro-de-un-rectangulo/index.js
@@ -0,0 +1,3 @@
+let baseRectangulo = prompt("Ingresá el valor de la base del rectangulo.");
+let alturaRectangulo= prompt("Ingresá el valor de la altura del rectangulo");
+alert("El resultado del perímetro del rectangulo es " + ((2* baseRectangulo) +(2 + alturaRectangulo)));
